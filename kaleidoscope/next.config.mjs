0aedfb37a1340a4d689540b8351c5308e617ac1d
newday <118717143+nwdy@users.noreply.github.com>
2024-05-19 10:27:40 +0700

    Fix some warnings

--- kaleidoscope/next.config.mjs
+++ kaleidoscope/next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['img.clerk.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        pathname: '/**',
+      },
+    ],
   },
   async headers() {
     return [
